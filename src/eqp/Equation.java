43e2ce4839602befcc671a48c5e8b33a0f80609b
Victor Chan <victorchan88888888@gmail.com>
2017-08-05 00:12:44 -0700

    Created Equation class

---
+++ src/eqp/Equation.java
@@ -0,0 +1,19 @@
+package eqp;
+
+/**
+ * Created by victorchan on 8/5/17.
+ */
+public class Equation {
+
+    private String original;
+    private String equation;
+
+    public Equation(String e) {
+        original = e;
+    }
+
+    public String getEquation() {
+        return equation;
+    }
+
+}
