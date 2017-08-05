6ad141394e3ff01e6c98d153628d56a0daa5fd2d
Victor Chan <victorchan88888888@gmail.com>
2017-08-05 00:23:33 -0700

    Added necessary functions

--- src/eqp/Equation.java
+++ src/eqp/Equation.java
@@ -1,5 +1,7 @@
 package eqp;
 
+import java.util.Stack;
+
 /**
  * Created by victorchan on 8/5/17.
  */
@@ -7,13 +9,28 @@ public class Equation {
 
     private String original;
     private String equation;
+    Stack<String> eq;
 
     public Equation(String e) {
         original = e;
+        equation = clean(original);
+        parse(equation, eq);
     }
 
     public String getEquation() {
         return equation;
+    }
+
+    public double evaluate(double n) {
+
+    }
+
+    private String clean(String o) {
+
+    }
+
+    private void parse(String e, Stack s) {
+
     }
 
 }
