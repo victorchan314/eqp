86afccfdca09e943c321200c9694bd52379fbd65
Victor Chan <victorchan88888888@gmail.com>
2017-08-05 03:32:21 -0700

    Added RegEx to check for Double

--- src/eqp/Equation.java
+++ src/eqp/Equation.java
@@ -3,6 +3,7 @@ package eqp;
 import java.util.Stack;
 import java.util.ArrayDeque;
 import java.util.HashMap;
+import java.util.regex.Pattern;
 
 /**
  * Created by victorchan on 8/5/17.
@@ -26,13 +27,45 @@ public class Equation {
     private static final String operator = String.join("|", operators.keySet()) + "|\\(|\\)";
     private static final String equationSeparator = "(?<=(" + operator + ") | (?=" + operator + "))";
 
+    private final String Digits     = "(\\p{Digit}+)";
+    private final String HexDigits  = "(\\p{XDigit}+)";
+    // an exponent is 'e' or 'E' followed by an optionally
+// signed decimal integer.
+    private final String Exp        = "[eE][+-]?"+Digits;
+    private final String fpRegex    =
+            ("[\\x00-\\x20]*"+ // Optional leading "whitespace"
+                    "[+-]?(" +         // Optional sign character
+                    "NaN|" +           // "NaN" string
+                    "Infinity|" +      // "Infinity" string
+                    // A decimal floating-point string representing a finite positive
+                    // number without a leading sign has at most five basic pieces:
+                    // Digits . Digits ExponentPart FloatTypeSuffix
+                    //
+                    // Since this method allows integer-only strings as input
+                    // in addition to strings of floating-point literals, the
+                    // two sub-patterns below are simplifications of the grammar
+                    // productions from the Java Language Specification, 2nd
+                    // edition, section 3.10.2.
+                    // Digits ._opt Digits_opt ExponentPart_opt FloatTypeSuffix_opt
+                    "((("+Digits+"(\\.)?("+Digits+"?)("+Exp+")?)|"+
+                    // . Digits ExponentPart_opt FloatTypeSuffix_opt
+                    "(\\.("+Digits+")("+Exp+")?)|"+
+                    // Hexadecimal strings
+                    "((" +
+                    // 0[xX] HexDigits ._opt BinaryExponent FloatTypeSuffix_opt
+                    "(0[xX]" + HexDigits + "(\\.)?)|" +
+                    // 0[xX] HexDigits_opt . HexDigits BinaryExponent FloatTypeSuffix_opt
+                    "(0[xX]" + HexDigits + "?(\\.)" + HexDigits + ")" +
+                    ")[pP][+-]?" + Digits + "))" +
+                    "[fFdD]?))" +
+                    "[\\x00-\\x20]*");// Optional trailing "whitespace"
+
     public Equation(String e) {
         checkEquation(e);
         original = e;
         equation = clean(original);
-        String[] args = equation.split(equationSeparator);
         eq = new ArrayDeque<>();
-        parse(equation, args, eq);
+        parse(equation.split(equationSeparator), eq);
     }
 
     public String getEquation() {
@@ -92,10 +125,11 @@ public class Equation {
         return o.replaceAll("\\s", "");
     }
 
-    private void parse(String e, String[] args, ArrayDeque eq) {
-        for (int i = 0; i < e.length(); i++) {
-            String t = Character.toString(e.charAt(i));
-            if (operators.containsKey(t)) {
+    private void parse(String[] args, ArrayDeque eq) {
+        for (String t : args) {
+            if (Pattern.matches(fpRegex, t)) {
+                eq.push(Double.valueOf(t));
+            } else if (operators.containsKey(t)) {
 
             } else if (t.equals("(")) {
 
