e9826c5322eead490392ecdefcc4e326c59b9602
Victor Chan <victorchan88888888@gmail.com>
2017-08-05 05:07:47 -0700

    Added testing code to Test class

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,13 @@
+import eqp.Equation;
 /**
  * Created by victorchan on 8/5/17.
  */
 public class Test {
+
+    public static void main(String[] args) {
+        String equation = "3 + 5";
+        Equation e = new Equation(equation);
+        e.evaluate(5);
+    }
+
 }
