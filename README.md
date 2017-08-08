b597574ee115d3a8ad8ada7f82d02637f86853d0
Victor Chan <victorchan88888888@gmail.com>
2017-08-08 00:08:30 -0700

    Updated README.md to be slightly more informative

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # eqp
-Equation parser in Java
+EQP is a simple equation parser in Java that uses the Shunting-yard algorithm to convert mathematic expressions into Reverse Polish notation.
--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,7 @@ import eqp.Equation;
 public class Test {
 
     public static void main(String[] args) {
-        String equation = "2*(9x+5)^0.5-1";
+        String equation = "4x";
         Equation e = new Equation(equation);
         System.out.println(e.evaluate(1));
         System.out.println(e.evaluate(5));
