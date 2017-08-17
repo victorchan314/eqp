3bd130da7ba0913578716ca4d0093845643c1094
Victor Chan <victorchan88888888@gmail.com>
2017-08-17 14:54:46 -0700

    Finished test for ln

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,7 @@ import eqp.Equation;
 public class Test {
 
     public static void main(String[] args) {
-        String equation = "ln(e)";
+        String equation = "ln(e^x)";
         Equation e = new Equation(equation);
         System.out.println(e.evaluate(1));
         System.out.println(e.evaluate(5));
